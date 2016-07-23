3f8131f44c4dbcf1c8281d59aaafb10c0179c81b
bansi-shah <bansishah0695@gmail.com>
2016-07-23 22:23:58 +0530

    Added Database entry

--- PHP_Scripts/menteesubmit.php
+++ PHP_Scripts/menteesubmit.php
@@ -1,5 +1,13 @@
 <?php
-	if(isset($_GET['feild1'])){
+	if(isset($_GET['field1']))
+	{
 		//enter into database
+		$field1 = $_GET['field1'];
+		echo "hi1";
+		$date = date();
+		// $id = $_SESSION['mentee_id'];
+
+		$id = 1; 
+		$sql = "INSERT INTO `katalyst`.`goals` (`goals_id`, `goal`, `mentee_id`, `status`, `mentor_id`, `created_by`, `edited_by`, `created_at`, `updated_at`) VALUES (NULL, $id, '$field1', '1', '1', CURRENT_TIMESTAMP, '$date', '1', '1');";
 	}
 ?>
